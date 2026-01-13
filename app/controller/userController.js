ce828bc4a4c705a8f3fb58c7644deb61d35d16fa
Mrunalinee1 <Mrunalineekhairnar@gmail.com>
2026-01-13 20:27:06 +0530

    added user code

---
+++ app/controller/userController.js
@@ -0,0 +1,38 @@
+const User = require('../models/user');
+const userService = require('../services/userService');
+
+class UserController {
+    getAllUsers(req, res) {
+        const users = userService.getAllUsers();
+        res.json(users);
+    }
+
+    getUserById(req, res) {
+        const id = parseInt(req.params.id, 10);
+        const user = userService.getUserById(id);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    }
+
+    createUser(req, res) {
+        const { id, name, email } = req.body;
+        const newUser = new User(id, name, email);
+        const createdUser = userService.createUser(newUser);
+        res.status(201).json(createdUser);
+    }
+
+    deleteUser(req, res) {
+        const id = parseInt(req.params.id, 10);
+        const deletedUser = userService.deleteUser(id);
+        if (deletedUser) {
+            res.json(deletedUser);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    }
+}
+
+module.exports = new UserController();
